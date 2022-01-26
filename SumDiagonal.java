fc9e22f3fafe657637eab9080ec017f81dc1dd13
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-26 23:33:17 +0530

    Create SumDiagonal.java
    Sum of the Diagonal Elements of an Matrix.(2D Array)

---
+++ SumDiagonal.java
@@ -0,0 +1,26 @@
+public class SumDiagonal {
+
+  //----------------- main method
+  public static void main(String[] args) {
+
+    //---------- declare and initialize a matrix
+    int a[][] =  { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+    //---------------- find sum of diagonal elements
+    int sum = diagonalSum(a);
+    
+    // ---------------display result
+    System.out.println("Sum of diagonal elements = " + sum);
+  }
+
+  // ----------method to find sum of diagonal elements of matrix
+  public static int diagonalSum(int[][] a) {
+    int sum = 0;
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[i].length; j++) {
+        if(i == j) sum += a[i][j];
+      }
+    }
+    return sum;
+  }
+}
